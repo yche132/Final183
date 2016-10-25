2fdbf4cdc42c30b32acd4ea030bea4d0eecdc63b
yuzhuang chen <yche132@ucsc.edu>
2016-10-25 15:32:01 -0700

    Add files via upload

---
+++ models/tables.py
@@ -0,0 +1,17 @@
+
+import datetime
+
+db.define_table('post',
+                Field('user_email', default=auth.user.email if auth.user_id else None),
+                Field('post_content', 'text'),
+                Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
+                Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
+                )
+
+
+db.post.user_email.readable = db.post.user_email.writable = False
+db.post.post_content.requires = IS_NOT_EMPTY()
+db.post.created_on.readable = db.post.created_on.writable = False
+db.post.updated_on.readable = db.post.updated_on.writable = False
+
+auth.enable_record_versioning(db)
